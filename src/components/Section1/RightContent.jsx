bd11a095fd6735ecb8b6cfebc22aa10d48d88693
Digvijay Singh <digvijaypundir915@gmail.com>
2025-11-01 21:27:05 +0530

    Update RightContent.jsx

--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -5,7 +5,7 @@ const RightContent = () => {
     <div>
       <div className='relative border-10 border-yellow-300 h-[70vh] w-[70vh] rounded-full overflow-hidden hover:border-yellow-700'>
         <div className='h-[70vh] w-[70vh] rounded-full flex items-center'>
-          <img id='profile' src="https://www.dropbox.com/scl/fi/y6behnoajlfn1ag6qjttz/digvijay_profile.jpg?rlkey=1n932vr2u14t4k2oerkfigmnd&st=uos3yvub&dl=0" alt="profile" />
+          <img id="profile" src="https://dl.dropboxusercontent.com/scl/fi/your_link_here?rlkey=xyz&st=abc" alt="profile" />
           <div className='absolute inset-0 bg-black/5 rounded-full h-[70vh] w-[70vh]'></div>
         </div>
       </div>
